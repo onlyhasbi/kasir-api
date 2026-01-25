a2075a46dc01c2e384e1621712785c6f66db1847
Muhammad Hasbi <hasbi@akasia.id>
2026-01-25 13:09:12 +0800

    add id in route

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	http.HandleFunc("GET /api/category", handlers.GetCategories)
 	http.HandleFunc("POST /api/category", handlers.PostCategory)
-	http.HandleFunc("GET /api/category/", handlers.GetCategory)
-	http.HandleFunc("DELETE /api/category/", handlers.DeleteCategory)
-	http.HandleFunc("PUT /api/category/", handlers.UpdateCategory)
+	http.HandleFunc("GET /api/category/{id}", handlers.GetCategory)
+	http.HandleFunc("DELETE /api/category/{id}", handlers.DeleteCategory)
+	http.HandleFunc("PUT /api/category/{id}", handlers.UpdateCategory)
 	http.HandleFunc("GET /health", handlers.GetHealthCheck)
 
 	fmt.Println("Server running di localhost:8080")
